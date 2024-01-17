23ad305946c89f4218dae456ed56e483543a7613
SOHEL RANA <sohelrana104322@gmail.com>
2024-01-18 01:32:05 +0600

    Add: sum of three numbers

---
+++ Problem Solving/add-three-numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c, sum;
+    cout << "Enter 1st number: ";
+    cin >> a;
+    cout << "Enter 2nd number: ";
+    cin >> b;
+    cout << "Enter 3rd number: ";
+    cin >> c;
+
+    sum = (a + b + c);
+    cout << "Sum of three numbers: " << sum;
+}
