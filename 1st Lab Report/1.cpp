91bf73f3d9ed86b4f648f11ccd752acedc4cc53d
SOHEL RANA <sohelrana104322@gmail.com>
2024-01-31 20:02:39 +0600

    insertion sort ascending order

---
+++ 1st Lab Report/1.cpp
@@ -0,0 +1,35 @@
+// ----insertion sort ascending order-------
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int size, i;
+    cout << "Enter size of an array: ";
+    cin >> size;
+    int myArray[size];
+
+    for(i = 0; i < size; i++){
+        cout << "Enter " << (i+1) << " element: ";
+        cin >> myArray[i];
+    }
+
+    for(i = 1; i < size; i++){
+        int temp = myArray[i];
+        int j = i - 1;
+
+        while (j >= 0 && myArray[j] > temp)
+        {
+            myArray[j+1] = myArray[j];
+            j--;
+        }
+        
+        myArray[j+1] = temp;
+    }
+
+    cout << "Inseration sort ascending order: ";
+    for(i = 0; i < size; i++){
+        cout << myArray[i] << " ";
+    }
+}
